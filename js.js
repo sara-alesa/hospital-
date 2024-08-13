7c65c2ba33e8047ab5d86d6a17be4625b06cb1e9
sara-alesa <saraelesa18@gmail.com>
2024-08-13 12:11:50 -0700

    Update js.js

---
+++ js.js
@@ -0,0 +1,81 @@
+function searchHospital() {
+    let searchQuery = document.querySelector('#search input').value.toLowerCase();
+    
+    let hospitalCards = document.querySelectorAll('.hospital-card');
+
+    hospitalCards.forEach(function(card) {
+        let hospitalName = card.getAttribute('data-name').toLowerCase();
+        let hospitalLocation = card.getAttribute('data-location').toLowerCase();
+
+        if (hospitalName.includes(searchQuery) || hospitalLocation.includes(searchQuery)) {
+            card.style.display = 'block'; 
+        } else {
+            card.style.display = 'none'; 
+        }
+    });
+}
+
+function viewDetails(button) {
+  
+    document.getElementById('results').style.display = 'none';
+    document.getElementById('search').style.display = 'none';
+
+
+    let hospitalCard = button.parentElement;
+
+
+    let hospitalName = hospitalCard.querySelector('h2').textContent;
+    let hospitalLocation = hospitalCard.getAttribute('data-location');
+    let hospitalImage = hospitalCard.querySelector('img').src;
+
+
+    document.getElementById('details-name').textContent = hospitalName;
+    document.getElementById('details-location').textContent = `الموقع: ${hospitalLocation}`;
+    document.getElementById('details-image').src = hospitalImage;
+
+  
+    const doctorsList = {
+        'مشفى الباب': [
+            { name: 'د. نوبليز', specialization: 'طب عام' },
+            { name: 'د.كنتاكي', specialization: 'جراحة' }
+        ],
+        'مشفى الفتح': [
+           
+        ],
+        'مشفى الفارابي': [
+            
+        ],
+        'مشفى الماجد': [
+            
+        ]
+    };
+
+    const doctors = doctorsList[hospitalName] || [];
+    const doctorsListElement = document.getElementById('details-doctors');
+    doctorsListElement.innerHTML = doctors.map(doc => `<li>${doc.name} - ${doc.specialization}</li>`).join('');
+
+
+    document.getElementById('hospital-details').style.display = 'block';
+}
+
+function closeDetails() {
+ 
+    document.getElementById('hospital-details').style.display = 'none';
+
+    document.getElementById('results').style.display = 'block';
+    document.getElementById('search').style.display = 'block';
+}
+
+function showSearch() {
+    document.getElementById('search').style.display = 'block';
+    document.getElementById('results').style.display = 'block';
+    document.getElementById('hospital-details').style.display = 'none';
+}
+
+function makeReservation() {
+    alert('تم النقر على زر الحجز.');
+}
+
+function contactHospital() {
+    alert('تم النقر على زر للتواصل.');
+}
